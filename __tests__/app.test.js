6d40f42c1a831a373ee653c17df1bede360e531b
ckaynatividad <natividad.ckay@gmail.com>
2022-03-15 18:26:32 -0700

    tests not passing for now

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('anyAPI routes', () => {
   beforeEach(() => {
@@ -11,4 +12,22 @@ describe('anyAPI routes', () => {
   afterAll(() => {
     pool.end();
   });
+});
+
+it('creates a cat', async () => {
+  const expected = {
+    name: 'Cardamon',
+    age: 4,
+    type: 'Himalayan',
+  };
+  const res = await request(app).post('/api/v1/cats').send(expected);
+
+  expect(res.body).toEqual({ id: 1, ...expected });
+});
+
+it('gets a list of cats', async () => {
+  const expected = await Cat.findALL();
+  const res = await request(app).get('/api/v1/cats');
+
+  expect(res.body).toEqual(expected);
 });
