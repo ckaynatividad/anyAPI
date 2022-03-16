63b9810257f75212a7e143368f1e8b26efdd1698
ckaynatividad <natividad.ckay@gmail.com>
2022-03-16 16:31:44 -0700

    all tests passing now

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -43,4 +43,45 @@ describe('anyAPI routes', () => {
 
     expect(res.body).toEqual(expected);
   });
+
+  it('gets cat by id', async () => {
+    const expected = {
+      name: 'Cardamon',
+      id: '1',
+      age: 4,
+      type: 'Himalayan',
+    };
+    const res = await request(app).get(`/api/v1/cats/${expected.id}`);
+    expect(res.body).toEqual(expected);
+  });
+
+  it('updates cat', async () => {
+    const cat = {
+      name: 'Cardamon',
+      id: '1',
+      age: 4,
+      type: 'Himalayan',
+    };
+
+    const res = await request(app)
+      .patch(`/api/v1/cats/${cat.id}`)
+      .send({ name: 'Cardo', age: 5, type: 'Siamese' });
+
+    const expected = {
+      id: expect.any(String),
+      name: 'Cardo',
+      age: 5,
+      type: 'Siamese',
+    };
+
+    expect(res.body).toEqual(expected);
+    expect(await Cat.findById(cat.id)).toEqual(expected);
+  });
+
+  it('deletes cat', async () => {
+    const expected = await Cat.findById(1);
+    const res = await request(app).delete(`/api/v1/cats/${expected.id}`);
+
+    expect(res.body).toEqual(expected);
+  });
 });
